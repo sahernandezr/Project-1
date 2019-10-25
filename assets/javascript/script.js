19d6634e3872f4eabaa9f90a1249500cb0d76d56
MikeStmria <likeitphoto@gmail.com>
2019-10-24 19:23:32 -0500

    Input selectors done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,10 +9,47 @@ $( document ).ready(function() {
     $(".datepicker").on("change", function() {
         var date = $(".datepicker").val();
         console.log("Date: " + date);
-    })
+    });
 
     $(".timepicker").on("change", function() {
         var time = $(".timepicker").val();
         console.log("Time: " + time);
-    })
+    });
+
+    // Location Snap
+    $(".locs").on("change", function() {
+        var locat = $(".locs").val();
+        console.log("Location: " + locat);
+    });
+
+
+    // ----- Functions -----
+
+    // Checkbox Function
+        // Checkbox blocked if other selected
+    $(".indoor").on("change", function() {
+
+        if ($(".outdoor").attr("disabled") === undefined) {
+            $(".outdoor").attr("disabled", true);
+        }
+
+        else if ($(".outdoor").attr("disabled") === "disabled") {
+            $(".outdoor").removeAttr("disabled");
+        }   
+    });
+
+    $(".outdoor").on("change", function() {
+
+        if ($(".indoor").attr("disabled") === undefined) {
+            $(".indoor").attr("disabled", true);
+        }
+        
+        else if ($(".indoor").attr("disabled") === "disabled") {
+            $(".indoor").removeAttr("disabled");
+        }   
+    });
+
+
+
+
 });
